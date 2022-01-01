fcef964ec87a9d5030a0d96d3a798bb3d0829b2e
Abdur Rahman Suzon <abdur.rahman@egeneration.co>
2022-01-01 13:28:46 +0600

    Fix minor issue

--- js/main.js
+++ js/main.js
@@ -1,29 +1,20 @@
 jQuery(document).ready(function () {
     let selectedSection;
-
     $(".menu-area-inner ul li").on("click", function () {
 
         selectedSection = $(this).children("a").attr("href").replace("#", "")
-
         currentActive = $(".menu-area-inner ul li.active").children("a").attr("href").replace("#", "");
-
         $(this).addClass("active");
         $(this).siblings().removeClass("active");
         // $(".page-container section .container").fadeOut(1);
-
         if (selectedSection !== currentActive) {
-
             $(".page-container section").removeClass("active-section");
             $(".page-container").find("#" + selectedSection).addClass("active-section");
-
-
         }
 
         //selectedSection = $(this).attr("href");
-        console.log(selectedSection)
-
+        // console.log(selectedSection)
         if (selectedSection === "portfolio") {
-
             setTimeout(function () {
                 $('.portfolio-wrapper').masonry({
                     itemSelector: '.portfolio-item',
@@ -31,7 +22,6 @@ jQuery(document).ready(function () {
                     percentPosition: true
                 })
             }, 1000)
-
         }
 
     })
